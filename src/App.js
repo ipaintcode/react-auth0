00a3aa12fb4555a86446f8831d14e891d8d995ac
Mark Learst <learst@me.com>
2018-12-03 01:31:01 -0500

    Added Callback class. Updated Auth.js to handle localStorage and added login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import Home from './components/Home'
-import Profile from './components/Profile'
-import Nav from './components/Nav'
+import React, { Component } from "react"
+import { Route } from "react-router-dom"
+import Home from "./components/Home"
+import Profile from "./components/Profile"
+import Nav from "./components/Nav"
+import Callback from "./components/Callback"
+import Auth from "./Auth/Auth"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.auth = new Auth(this.props.history)
+  }
   render() {
     return (
       <>
-        <Nav />
+        <Nav auth={this.auth} />
         <div className="body">
-          <Route path="/" exact component={Home} />
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
           <Route path="/profile" component={Profile} />
         </div>
       </>
